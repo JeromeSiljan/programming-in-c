8ca15ad29bea2482680ca686939b2938ac029b35
Jerome Siljan <nightfoxcoc73@gmail.com>
2020-11-17 11:30:07 -0600

    finished chapter 2

---
+++ 2 - Compiling And Running Your First Program/prog1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  //allows for functions such as printf, almost always needed for a C program
+
+int main (void) //name of program is main and returns int. main is the name of a function that takes no arguments (void). 
+{
+    printf("Programming is fun\n");
+
+    return 0;    //announce that program is finished. 0 is used by convention to indicate that there were no ers
+}
